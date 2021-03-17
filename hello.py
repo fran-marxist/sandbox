22c621860891c91d0690736c667aa79a2cdc9347
fran-marxist <francisco.silveira.chaves@gmail.com>
2021-03-17 20:03:04 +0000

    pitagoras

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello!")
+from math import sqrt
+
+A = (1121231231312312312323123132)
+B = (3312312312132131231233232)
+a = (43212231212312331233331)
+b = (85853212312331231231851)
+Xdistance = (a-A)
+Ydistance = (b-B)
+Diagonaldistance = (Ydistance*Ydistance+Xdistance*Xdistance) 
+Diagonaldistancenotsquared = sqrt(Diagonaldistance)
+print (Diagonaldistancenotsquared)
