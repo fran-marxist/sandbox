14e8bda2cf73ed24ed8770063b8ffdd77c3bb840
fran-marxist <francisco.silveira.chaves@gmail.com>
2021-06-01 02:49:23 +0000

    done

--- percentage.py
+++ percentage.py
@@ -1,8 +1,13 @@
-numerator = 4
-denominator = 12
+tries = 0
+numerator = 1
+denominator = 10
 percentage = 100/(denominator/numerator)
-print (percentage)
+print (f'percentage = {percentage}')
+print ( )
 percentageTries = 100
 while percentageTries > 50:
-    print (percentageTries/(denominator/numerator))
-    percentageTries = percentageTries/(denominator/numerator)
+    print (percentageTries*((100-percentage)/100))
+    percentageTries = percentageTries*((100-percentage)/100)
+    tries = tries + 1
+print ( )
+print (f' You would have to play this lottery {tries} times to have a good chance of winning.')
