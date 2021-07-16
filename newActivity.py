00d92894c061ca11f2038c9e50855b3328c483c5
fran-marxist <francisco.silveira.chaves@gmail.com>
2021-07-16 23:06:16 +0000

    Added interactive feature to percentage.py.

---
+++ newActivity.py
@@ -0,0 +1,7 @@
+import sys
+
+x = int(sys.argv[1])
+if (x == 1):
+    print ("x = 1")
+else: 
+    print ("x =! 1")
--- percentage.py
+++ percentage.py
@@ -1,13 +1,16 @@
+import sys
 tries = 0
-numerator = 1
-denominator = 10
+numerator = int(sys.argv[1])
+denominator = int(sys.argv[2])
 percentage = 100/(denominator/numerator)
+desiredPercentageInverted = int(sys.argv[3])
+desiredPercentageNotInverted = (100 - desiredPercentageInverted)
 print (f'percentage = {percentage}')
 print ( )
 percentageTries = 100
-while percentageTries > 50:
+while percentageTries > desiredPercentageNotInverted:
     print (100-(percentageTries*((100-percentage)/100)))
     percentageTries = percentageTries*((100-percentage)/100)
     tries = tries + 1
 print ( )
-print (f' You would have to play this lottery {tries} times to have a good chance of winning.')
+print (f'You would have to play this lottery {tries} times to have a good chance of winning.')
