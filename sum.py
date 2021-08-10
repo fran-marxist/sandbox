2f60ecfc2c4477b5240c3b6efdce6796b318b06c
fran-marxist <francisco.silveira.chaves@gmail.com>
2021-08-10 17:48:30 +0000

    Finished sum.py

---
+++ sum.py
@@ -0,0 +1,9 @@
+number = 15
+numberAdapted = (number - 1)
+sum = 0
+while (numberAdapted != 1):
+    sum = (sum + numberAdapted)
+    numberAdapted = (numberAdapted - 1)
+    print (numberAdapted)
+sum = (sum + numberAdapted)
+print (sum)
